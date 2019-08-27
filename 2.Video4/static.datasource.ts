2e19b09444918ed6b8dad79c290811e2dd383eed
Giancarlo Gamarra <ggamarra@solucionsmart.com>
2019-08-26 23:00:57 -0500

    Update static.datasource.ts

--- 2.Video4/static.datasource.ts
+++ 2.Video4/static.datasource.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Product } from './product.model';
 import { Observable, from } from 'rxjs';
-import { Order } from './order.model';
 @Injectable()
 export class StaticDataSource {
     private products: Product[] = [
